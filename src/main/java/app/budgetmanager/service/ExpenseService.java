cdfe0c1e36874644f6aa445bdf6a88e8206db0f3
Ivan Shevtsov <vanyshevtsov2@gmail.com>
2026-03-03 12:49:11 +0300

    Update ExpenseService.java

--- src/main/java/app/budgetmanager/service/ExpenseService.java
+++ src/main/java/app/budgetmanager/service/ExpenseService.java
@@ -9,7 +9,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class ExpenseService {
@@ -46,5 +45,6 @@ public class ExpenseService {
                 .map(expenseMapper::toDto);
     }
 }
+
 
 
